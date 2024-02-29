77d2b9a98a976e5fe705d5b951b352eaa9a28076
spacehee <woojh9504@gmail.com>
2024-02-29 17:57:39 +0900

    weatherIconCOMPLETE
    날씨 아이콘 띄우기 완성

--- Assets/Scripts/Lobby/CurrentInfo/WeatherAPI.cs
+++ Assets/Scripts/Lobby/CurrentInfo/WeatherAPI.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.UI;
+using UnityEngine.UI;  // Image를 사용하기 위해 추가
 
 public class WeatherAPI : MonoBehaviour
 {
-
-
+    public Image weatherImage;  // SpriteRenderer 대신 Image를 사용
     public Text temperatureText;
 
     void Start()
@@ -34,31 +33,61 @@ public class WeatherAPI : MonoBehaviour
             }
         }
     }
-    void ProcessWeatherInfo(string response)
-    {
 
+    public void ProcessWeatherInfo(string response)
+    {
         WeatherData weatherData = JsonUtility.FromJson<WeatherData>(response);
 
-        //temp(온도) 단위가kelvin
+        //temp(온도) 단위가 kelvin
         float kelvinTemperature = weatherData.main.temp;
 
         // kelvin -> 섭씨
         float celsiusTemperature = kelvinTemperature - 273.15f;
 
+        temperatureText.text = "Seoul : " + celsiusTemperature.ToString("F2") + "°C";
 
-        temperatureText.text = celsiusTemperature.ToString("F2") + "°C";
+        int weatherId = weatherData.weather[0].id;
+        Sprite weatherSprite = GetWeatherIcon(weatherId);
+
+        // Image의 sprite 속성에 직접 할당
+        weatherImage.sprite = weatherSprite;
     }
 
+    // 아이콘 조건
+    Sprite GetWeatherIcon(int weatherId)
+    {
+        string iconName = "";
+
+        if (weatherId >= 200 && weatherId <= 299) iconName = "thunder"; // Thunderstorm
+        else if (weatherId >= 300 && weatherId <= 399) iconName = "drizzle"; // Drizzle
+        else if (weatherId >= 500 && weatherId <= 599) iconName = "Rain"; // Rain
+        else if (weatherId >= 600 && weatherId <= 699) iconName = "snow"; // Snow
+        else if (weatherId >= 700 && weatherId <= 799) iconName = "fog"; // fog
+        else if (weatherId >= 801 && weatherId <= 899) iconName = "cloud"; // Clouds
+        else iconName = "sun";
+
+        // Resources 폴더 아래의 Icons 폴더에서 이미지를 불러오기
+        Sprite weatherSprite = Resources.Load<Sprite>("Icons/" + iconName);
+
+        return weatherSprite;
+    }
 
     [System.Serializable]
     public class WeatherData
     {
         public Main main;
+        public Weather[] weather;
     }
 
     [System.Serializable]
     public class Main
     {
         public float temp;
+    }
+
+    [System.Serializable]
+    public class Weather
+    {
+        public int id;
     }
 }
--- Assets/Scripts/Solaseado/ArduinoControl.cs
+++ Assets/Scripts/Solaseado/ArduinoControl.cs
@@ -1,57 +1,57 @@
-using UnityEngine;
-using System.IO.Ports;
+//using UnityEngine;
+//using System.IO.Ports;
 
 
-public class ArduinoControl : MonoBehaviour
-{
-    SerialPort stream = new SerialPort("COM3", 9600); // 포트와 보드레이트 설정
-    public GameObject panel; // 로테이션을 적용할 오브젝트
+//public class ArduinoControl : MonoBehaviour
+//{
+//    SerialPort stream = new SerialPort("COM3", 9600); // 포트와 보드레이트 설정
+//    public GameObject panel; // 로테이션을 적용할 오브젝트
 
-    private Quaternion prevRotation; // 이전 로테이션을 저장하는 변수
+//    private Quaternion prevRotation; // 이전 로테이션을 저장하는 변수
 
-    void Start()
-    {
-        stream.Open(); // 시리얼 포트 열기
-        prevRotation = panel.transform.rotation; // 초기 로테이션 값 저장
-    }
+//    void Start()
+//    {
+//        stream.Open(); // 시리얼 포트 열기
+//        prevRotation = panel.transform.rotation; // 초기 로테이션 값 저장
+//    }
 
-    void Update()
-    {
-        if (stream.IsOpen)
-        {
-            try
-            {
-                // 서보 모터 각도를 받아오기
-                string value = stream.ReadLine();
-                float angle = float.Parse(value);
+//    void Update()
+//    {
+//        if (stream.IsOpen)
+//        {
+//            try
+//            {
+//                // 서보 모터 각도를 받아오기
+//                string value = stream.ReadLine();
+//                float angle = float.Parse(value);
 
-                // 800 이하의 값에 대해서만 0도로 돌아오도록 설정
-                if (angle <= 800)
-                {
-                    panel.transform.rotation = Quaternion.Euler(0, 0, 0);
-                }
-                // 800 초과의 값에 대해서만 60도로 설정
-                else if (angle > 800 && angle != 60)
-                {
-                    panel.transform.rotation = Quaternion.Euler(0, 60, 0);
-                }
+//                // 800 이하의 값에 대해서만 0도로 돌아오도록 설정
+//                if (angle <= 800)
+//                {
+//                    panel.transform.rotation = Quaternion.Euler(0, 0, 0);
+//                }
+//                // 800 초과의 값에 대해서만 60도로 설정
+//                else if (angle > 800 && angle != 60)
+//                {
+//                    panel.transform.rotation = Quaternion.Euler(0, 60, 0);
+//                }
 
-                // 로테이션 값이 변경된 경우에만 Debug.Log를 출력
-                if (panel.transform.rotation != prevRotation)
-                {
-                    Debug.Log("현재 회전 각도: " + panel.transform.rotation.eulerAngles.y);
-                    prevRotation = panel.transform.rotation;
-                }
-            }
-            catch (System.Exception)
-            {
-                // 예외 처리 - 잘못된 형식의 데이터를 읽을 경우
-            }
-        }
-    }
+//                // 로테이션 값이 변경된 경우에만 Debug.Log를 출력
+//                if (panel.transform.rotation != prevRotation)
+//                {
+//                    Debug.Log("현재 회전 각도: " + panel.transform.rotation.eulerAngles.y);
+//                    prevRotation = panel.transform.rotation;
+//                }
+//            }
+//            catch (System.Exception)
+//            {
+//                // 예외 처리 - 잘못된 형식의 데이터를 읽을 경우
+//            }
+//        }
+//    }
 
-    void OnDestroy()
-    {
-        stream.Close(); // 프로그램 종료 시 포트 닫기
-    }
-}
+//    void OnDestroy()
+//    {
+//        stream.Close(); // 프로그램 종료 시 포트 닫기
+//    }
+//}
