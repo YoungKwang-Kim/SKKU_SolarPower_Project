5b27d950875c2acb8b67e758329662a89a80e1ab
YoungKwang-Kim <kyk01910691@gmail.com>
2024-03-05 12:48:16 +0900

    솔라시도씬 태양의 움직임 추가 및 드론 프로펠러 애니메이션 추가

--- Assets/Scripts/Solaseado/ReloadSunDataAndRotate.cs
+++ Assets/Scripts/Solaseado/ReloadSunDataAndRotate.cs
@@ -13,8 +13,7 @@ public class ReloadSunDataAndRotate : MonoBehaviour
     // sun의 부모 오브젝트, 태양의 남중고도만큼 x축을 회전시킨다.
     public Transform sunAltitude;
     public Light sun;
-    public TMP_InputField dateInput;
-    public Button button;
+    public Text dateText;
     public Slider slider;
 
     private SunData sunData;
@@ -22,21 +21,11 @@ public class ReloadSunDataAndRotate : MonoBehaviour
 
     void Start()
     {
-        // 현재 날짜를 dateInput에 입력
-        dateInput.text = DateTime.Now.ToString("yyyyMMdd");
         // 슬라이더의 기본값은 0.5
-        slider.value = 0.5f;
-        // 버튼 클릭 이벤트에 메서드 연결
-        button.onClick.AddListener(OnButtonClick);
+        slider.value = 12f;
         // 슬라이더 값 변경 이벤트에 메서드 연결
         slider.onValueChanged.AddListener(OnSliderValueChanged);
         // 초기 데이터 로드 및 태양 위치 업데이트
-        FetchSunData();
-    }
-    // 버튼 클릭 시 호출되는 메서드
-    private void OnButtonClick()
-    {
-        // 사용자가 선택한 날짜로 데이터를 다시 가져와서 태양 위치 업데이트
         FetchSunData();
     }
     // 슬라이더로 태양 회전
@@ -49,12 +38,14 @@ public class ReloadSunDataAndRotate : MonoBehaviour
     /// <summary>
     /// 태양의 위도, 고도 데이터 받아서 저장
     /// </summary>
-    private void FetchSunData()
+    public void FetchSunData()
     {
+        string _dateText = dateText.text.Replace("-", "");
         string url = new UriBuilder("http://apis.data.go.kr/B090041/openapi/service/SrAltudeInfoService/getAreaSrAltudeInfo")
         {
-            Query = $"ServiceKey={serviceKey}&location=서울&locdate={dateInput.text}"
+            Query = $"ServiceKey={serviceKey}&location=서울&locdate={_dateText}"
         }.Uri.ToString();
+        Debug.Log(dateText.text);
 
         sunData = new SunData();
 
@@ -74,16 +65,16 @@ public class ReloadSunDataAndRotate : MonoBehaviour
         }
 
         // 남중고도일 때의 태양 표현
-        rotateSunAndPanel(0.5f);
+        rotateSunAndPanel(12f);
         // 슬라이더의 기본값은 0.5
-        slider.value = 0.5f;
+        slider.value = 12f;
     }
 
     // 태양과 태양광 패널 회전
     private void rotateSunAndPanel(float value)
     {
         sunAltitude.rotation = Quaternion.Euler(new Vector3(-sunData.altitudeMeridian, 0, 0));
-        sun.transform.localRotation = Quaternion.AngleAxis(value * 360, Vector3.up);
+        sun.transform.localRotation = Quaternion.AngleAxis(value / 24 * 360, Vector3.up);
 
         // 패널 회전부분 구현해야함.
     }
